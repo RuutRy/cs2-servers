1bb166078e47a6703c5ec98fafcfe3aefd9136c0
ekosfin <elias.samuli@gmail.com>
2024-03-21 15:00:45 +0200

    removed cs

--- infra/main.tf
+++ infra/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_subnet" "cs_subnet" {
 # ------------------------------------------------------------------------------------------------------
 
 resource "azurerm_dns_zone" "games_ruut" {
-  name                = "games.ruut.me"
+  name                = "pelit.ruut.me"
   resource_group_name = azurerm_resource_group.rg.name
 }
 
@@ -47,7 +47,7 @@ resource "cloudflare_record" "name_servers" {
   for_each = azurerm_dns_zone.games_ruut.name_servers
 
   zone_id = var.cloudflare_zone_id
-  name    = "games.ruut.me"
+  name    = "pelit.ruut.me"
   type    = "NS"
   value   = each.value
 
