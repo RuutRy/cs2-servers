53e10b05894618eb6865cb5449c664b86e271929
ekosfin <elias.samuli@gmail.com>
2025-02-24 09:06:10 +0200

    Update ubuntu version

--- infra/module/virtual_machine/main.tf
+++ infra/module/virtual_machine/main.tf
@@ -76,7 +76,7 @@ resource "azurerm_linux_virtual_machine" "game" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    sku       = "24_04-lts"
     version   = "latest"
   }
 }
